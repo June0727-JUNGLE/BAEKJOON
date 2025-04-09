1e369d570c5ed42bc3fccea00264f8375d29c3d6
June0727-JUNGLE <wlwnsqo0727@gmail.com>
2025-04-09 15:19:44 +0900

    [Silver I] Title: 신입 사원, Time: 3680 ms, Memory: 60108 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    applicants = [tuple(map(int, input().split())) for _ in range(n)]
+
+    applicants.sort()
+
+    count = 1
+    min_interview = applicants[0][1]
+
+    for i in range(1, n):
+        if applicants[i][1] < min_interview:
+            count += 1
+            min_interview = applicants[i][1]
+
+    print(count)
